8a0c17d36fee3cf40a70da2db71fbd151e4d8fe2
Adam <8365885+zvakanaka@users.noreply.github.com>
2015-01-20 21:45:15 -0700

    added web pages

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Aquinton | Home</title>
+    <link rel="favorite icon" type="image/x-icon" href="favicon.ico" />
+        <link href="css/tanga.css" type="text/css" rel="stylesheet" media="screen">
+  </head>
+  <body>
+<?php
+ini_set('display_errors', 'On');
+error_reporting(E_ALL);
+include 'header.php'
+//echo readfile("index.php");
+?>
+    <div>
+    <article>
+  <h1>CS313 - The Best Class at BYU-I</h1>
+  <img src="images/terminal.png" width="150" alt="Terminal" />
+  <p>The goal of this page is to teach you to use a terminal.  After years of practice, you can master that command in the image to the left.</p>
+</article>
+<br /><br />
+<?php include 'footer.php' ?>
+</div>
+</body>
+</html>
