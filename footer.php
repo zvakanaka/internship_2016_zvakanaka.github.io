e70b077298eead0bac5a54ef0e6c4d23cb7defd1
Adam <8365885+zvakanaka@users.noreply.github.com>
2016-04-30 20:30:04 -0600

    Change to Jekyll

--- footer.php
+++
@@ -1,8 +0,0 @@
-<footer>
-  <nav>
-    <ul>
-    <li>Contact: Adam Quinton <a href="mailto:qui10001@byui.edu">Email</a> </li>
-      <li><a href="http://www.github.com/zvakanaka">Github</a> </li>  
-    </ul>
-  </nav>
-</footer>
